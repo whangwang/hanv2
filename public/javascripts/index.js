6dba3ac4d0cde94931dbb09346783edcddcaf54f
王瀚 <whangwang0430@gmail.com>
2018-10-24 11:43:38 +0800

    fix style in mobile

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,20 @@
 $(document).mousemove(function(e){
-    $('#cursor').css('left',e.clientX);
-    $('#cursor').css('top',e.clientY);
+    if( $(window).width() > 1023 ){
+      $('#cursor').css('left',e.clientX);
+      $('#cursor').css('top',e.clientY);
+    }
   });
   $(document).click(function(e){
-    let eff = $('<div class="clickEff" style="left: '+e.clientX+'px;top: '+e.clientY+'px;"></div>');
-    $(document.body).append(eff);
-    let start = setTimeout(function(){
-      eff.addClass('start');
-    },10);
-    let timeout = setTimeout(function(){
-      eff.remove();
-    },500);
+    if( $(window).width() > 1023 ){
+      let eff = $('<div class="clickEff" style="left: '+e.clientX+'px;top: '+e.clientY+'px;"></div>');
+      $(document.body).append(eff);
+      let start = setTimeout(function(){
+        eff.addClass('start');
+      },10);
+      let timeout = setTimeout(function(){
+        eff.remove();
+      },500);
+    }
   });
 $(document).ready(function(){
     $('button, .scrollPrompt>p, .scrollPrompt>svg, a, .navbar>img, .cateNavbar>img, .cateNavbar>div>img').hover(function(){
